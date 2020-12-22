15f5bb5b3e59e1e81afa56e1fbe4802eeca49f4f
mmsk <rovnyagin@gmail.com>
2020-12-22 17:56:31 +0300

    lab6

---
+++ run-spark.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+bin/spark-submit \
+   --master k8s://https://$(kubectl get node minikube -o jsonpath='{.status.addresses[0].address}'):8443 \
+   --deploy-mode client \
+   --name spark-pi \
+   --class org.apache.spark.examples.SparkPi \
+   --conf spark.executor.instances=2 \
+   --conf spark.kubernetes.container.image=k8s2/spark \
+   --conf spark.kubernetes.namespace=default \
+   local:///opt/spark/examples/jars/spark-examples_2.11-2.4.4.jar
---
+++ spark-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+wget https://archive.apache.org/dist/spark/spark-2.4.4/spark-2.4.4-bin-hadoop2.7.tgz
+tar -xvf spark-2.4.4-bin-hadoop2.7.tgz
+sudo mv spark-2.4.4-bin-hadoop2.7 /opt/spark
+cd /opt/spark
+echo 'RUN rm $SPARK_HOME/jars/kubernetes-client-4.1.2.jar' >> kubernetes/dockerfiles/spark/Dockerfile
+echo 'ADD https://repo1.maven.org/maven2/io/fabric8/kubernetes-client/4.5.1/kubernetes-client-4.5.1.jar $SPARK_HOME/jars' >> kubernetes/dockerfiles/spark/Dockerfile
+./bin/docker-image-tool.sh -r k8s -m build
+kubectl create clusterrolebinding default --clusterrole=edit --serviceaccount=default:default --namespace=default
